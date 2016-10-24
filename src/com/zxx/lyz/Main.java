c6c3dad7439b8a4c6d0a6c5f578066fe2774732e
tzjtatata <tzjtatata@qq.com>
2016-10-24 14:49:03 +0800

    Add the database file

--- src/com/zxx/lyz/Main.java
+++ src/com/zxx/lyz/Main.java
@@ -10,7 +10,7 @@ public class Main {
         ArrayList<Data> dataBase = readSet.readTrainingData(1000);
         Formal formalUnit = new Formal(dataBase);
         ArrayList<Data> formalizedDatabase = formalUnit.getResult();
-        KDTree kdTree = new KDTree(42, formalizedDatabase);
+        KDTree kdTree = new KDTree(41, formalizedDatabase);
         ArrayList<Data> testingDataBase = formalUnit.formalWithOldParams(readSet.readTestingData(1000));
         Conner connerUnit = new Conner(testingDataBase, kdTree, 0.5);
         double accuracy = connerUnit.test();
